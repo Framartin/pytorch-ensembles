f21b28f6706c9f9a5169c52f426e7a010e0bae3d
Martin G <1850174+Framartin@users.noreply.github.com>
2020-09-02 17:36:31 +0200

    remove copie

---
+++ models/torchvision.py
@@ -0,0 +1,12 @@
+from torchvision import models
+
+class cifar50cifar:
+    base = models.
+    args = list()
+    kwargs = {'depth': 28, 'widen_factor': 10}
+
+
+class WideResNet28x10do:
+    base = WideResNet
+    args = list()
+    kwargs = {'depth': 28, 'widen_factor': 10, 'dropout_rate': 0.3}
