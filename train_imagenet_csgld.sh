927567400cfae3cb6087eede6ef95670af379c79
Martin G <1850174+Framartin@users.noreply.github.com>
2020-11-14 01:02:47 +0100

    change computing params

--- train_imagenet_csgld.sh
+++ train_imagenet_csgld.sh
@@ -5,9 +5,9 @@
 #SBATCH --partition=gpu # Use the batch partition reserved for passive jobs
 #SBATCH -J cSGLDImageNet  # Set the job name
 #SBATCH -N 1              # 1 computing nodes
-#SBATCH -n 2              # 2 tasks
-#SBATCH -c 8              # 8 cores per task
-#SBATCH --gpus 1          # 4 GPU per tasks
+#SBATCH -n 1              # 1 tasks
+#SBATCH -c 16             # 16 cores per task
+#SBATCH --gpus 2          # 2 GPU per tasks
 #SBATCH -C volta          # fix type of GPU to compare runtime
 #SBATCH -o "log/run_train_imagenet_csgld_%j.log"
 #SBATCH --mail-type=end,fail
@@ -29,8 +29,8 @@ SAMPLES_PER_CYCLE=3
 #WORKERS=16
 #PRINT_FREQ=400
 #debug:
-BATCH_SIZE=64
-PRINT_FREQ=20
+BATCH_SIZE=32
+PRINT_FREQ=10
 WORKERS=8
 
 
