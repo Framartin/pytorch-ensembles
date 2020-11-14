f745af7c1831d2ae79bb6bf10efaa5e63fe10de5
Martin G <1850174+Framartin@users.noreply.github.com>
2020-11-14 01:55:35 +0100

    remove debug option

--- train_imagenet_csgld.sh
+++ train_imagenet_csgld.sh
@@ -1,19 +1,18 @@
 #!/bin/bash -l
 # DEBUG
-#SBATCH --time=0-01:00:00 # 1 hours
-#SBATCH --qos=besteffort
-#SBATCH --partition=gpu # Use the batch partition reserved for passive jobs
+#SBATCH --time=8-00:00:00 # 8 days
+#SBATCH --partition=gpu   # Use the batch partition reserved for passive jobs
 #SBATCH -J cSGLDImageNet  # Set the job name
 #SBATCH -N 1              # 1 computing nodes
 #SBATCH -n 1              # 1 tasks
 #SBATCH -c 16             # 16 cores per task
-#SBATCH --gpus 2          # 2 GPU per tasks
+#SBATCH --gpus 4          # 4 GPU per tasks
 #SBATCH -C volta          # fix type of GPU to compare runtime
 #SBATCH -o "log/run_train_imagenet_csgld_%j.log"
 #SBATCH --mail-type=end,fail
 
 echo
-echo "cSGLD assumes a multi-GPU training, in our case 1 sample tooks 15-17 hours (45 epoch) on 4 v100."
+echo "cSGLD assumes a multi-GPU training, in our case 1 cycle tooks 15-17 hours (45 epochs) on 4 v100."
 echo
 
 command -v module >/dev/null 2>&1 && module load lang/Python
@@ -25,13 +24,13 @@ DATAPATH="/work/projects/bigdata_sets/ImageNet/ILSVRC2012/raw-data/"
 LR=0.1
 CYCLES=10
 SAMPLES_PER_CYCLE=3
-#BATCH_SIZE=256
-#WORKERS=16
-#PRINT_FREQ=400
+BATCH_SIZE=256
+WORKERS=16
+PRINT_FREQ=400
 #debug:
-BATCH_SIZE=32
-PRINT_FREQ=10
-WORKERS=8
+#BATCH_SIZE=32
+#PRINT_FREQ=10
+#WORKERS=8
 
 
 # 1 node with 4 GPUs and 16 cpus (will use as much GPUs available on the node)
