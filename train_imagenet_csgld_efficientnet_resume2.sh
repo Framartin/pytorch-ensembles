ea1dccc570b344f8c2832d351d3e39bd123fb14a
Martin G <1850174+Framartin@users.noreply.github.com>
2020-12-24 13:39:35 +0100

    sbatch config fix

--- train_imagenet_csgld_efficientnet_resume2.sh
+++ train_imagenet_csgld_efficientnet_resume2.sh
@@ -2,13 +2,13 @@
 #SBATCH --time=6-12:00:00 # approx 110 hours / cycle
 #SBATCH --partition=gpu   # Use the batch partition reserved for passive jobs
 #SBATCH --qos=long
-#SBATCH -J cSGLDeffnetb0  # Set the job name
+#SBATCH -J cSGLDeffnet    # Set the job name
 #SBATCH -N 1              # 1 computing nodes
 #SBATCH -n 1              # 1 tasks
 #SBATCH -c 8              # 8 cores per task
 #SBATCH --gpus 1          # 1 GPU per tasks
 #SBATCH -C volta          # fix type of GPU to compare runtime
-#SBATCH -o "log/run_train_imagenet_csgld_efficientnet_resume_%j.log"
+#SBATCH -o "log/run_train_imagenet_csgld_efficientnet_resume2_%j.log"
 #SBATCH --mail-type=begin,end,fail
 
 echo
@@ -24,14 +24,13 @@ ARCH="efficientnet_b0"
 LR=0.01  # resume after 1rst cycle with smaller LR
 CYCLES=3
 SAMPLES_PER_CYCLE=3
-BATCH_SIZE=256 # increase batch-size for efficency
+BATCH_SIZE=256 # increase batch-size for efficiency
 WORKERS=8
 PRINT_FREQ=400
 DIR="../models/ImageNet/${ARCH}/cSGLD_cycles${CYCLES}_samples${SAMPLES_PER_CYCLE}_bs${BATCH_SIZE}"
 
 date
 
-# efficientnet B0 should take 1h16 / epoch on 1 V100
 python -u train_imagenet_csgld.py --data $DATAPATH --no-normalization --arch $ARCH \
   --export-dir $DIR --workers $WORKERS --batch-size $BATCH_SIZE \
   --lr $LR --max-lr $LR --print-freq $PRINT_FREQ --world-size 1 \
