b68c7fc7737959956bd63276a0c0a3277675f2a0
Martin G <1850174+Framartin@users.noreply.github.com>
2020-12-25 02:12:42 +0100

    add train_imagenet_csgld_wide.sh

---
+++ train_imagenet_csgld_wide.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+# bash train_imagenet_csgld_wide.sh >>log/run_train_imagenet_csgld_wide.log 2>&1
+
+echo
+echo "single GPU training - manual scheduling"
+echo
+
+source /opt/miniconda/bin/activate
+conda activate pytorch
+
+set -x
+
+#specify GPU
+CUDA_VISIBLE_DEVICES=1
+
+DATAPATH="../../data/ILSVRC2012"
+ARCH="wide_resnet50_2"
+LR=0.1  # original paper
+WEIGHT_DECAY=0.0005  # original paper
+CYCLES=3
+SAMPLES_PER_CYCLE=3
+BATCH_SIZE=256
+WORKERS=10
+PRINT_FREQ=400
+DIR="../models/ImageNet/${ARCH}/cSGLD_cycles${CYCLES}_samples${SAMPLES_PER_CYCLE}_bs${BATCH_SIZE}_lr${LR}_wd${WEIGHT_DECAY}"
+
+
+date
+
+python -u train_imagenet_csgld.py --data $DATAPATH --no-normalization --arch $ARCH \
+  --export-dir $DIR --workers $WORKERS --batch-size $BATCH_SIZE \
+  --lr $LR --max-lr $LR --print-freq $PRINT_FREQ --world-size 1 \
+  --cycles $CYCLES --cycle-epochs 45 --samples-per-cycle $SAMPLES_PER_CYCLE --noise-epochs $SAMPLES_PER_CYCLE \
+  --gpu $CUDA_VISIBLE_DEVICES \
+  --weight-decay $WEIGHT_DECAY
+
+date
