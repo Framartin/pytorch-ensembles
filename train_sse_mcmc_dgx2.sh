c8c8fc181af5e098ccc6b7dca1fa5084402e1653
Martin G <1850174+Framartin@users.noreply.github.com>
2021-01-12 14:45:16 +0100

    add train_sse_mcmc_dgx2.sh

---
+++ train_sse_mcmc_dgx2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+# launch with:
+# bash train_sse_mcmc_dgx2.sh >>log/cifar10/train_sse_mcmc_dgx2.log 2>&1
+
+source /opt/miniconda/bin/activate
+conda activate pytorch
+
+set -x
+
+#specify GPU
+export CUDA_VISIBLE_DEVICES=2
+
+#bash train_sse_mcmc.sh CIFAR10 VGG16BN 1 ../models ../data cSGLD
+
+#bash train_sse_mcmc.sh CIFAR10 VGG19BN 1 ../models ../data cSGLD
+
+#bash train_sse_mcmc.sh CIFAR10 PreResNet164 1 ../models ../data cSGLD
+# 250 epochs in 323 minutes
+
+
+bash train_sse_mcmc.sh CIFAR10 WideResNet28x10 1 ../models ../data cSGLD
