2045fdadcf482d04ac14dd6fb319361b26194bec
Martin G <1850174+Framartin@users.noreply.github.com>
2021-01-12 14:45:36 +0100

    tmp update train_sse_mcmc_dgx.sh

--- train_sse_mcmc_dgx.sh
+++ train_sse_mcmc_dgx.sh
@@ -11,12 +11,12 @@ set -x
 #specify GPU
 export CUDA_VISIBLE_DEVICES=1
 
-bash train_sse_mcmc.sh CIFAR10 VGG16BN 1 ../models ../data cSGLD
+#bash train_sse_mcmc.sh CIFAR10 VGG16BN 1 ../models ../data cSGLD
 
-bash train_sse_mcmc.sh CIFAR10 VGG19BN 1 ../models ../data cSGLD
+#bash train_sse_mcmc.sh CIFAR10 VGG19BN 1 ../models ../data cSGLD
 
 bash train_sse_mcmc.sh CIFAR10 PreResNet164 1 ../models ../data cSGLD
 # 250 epochs in 323 minutes
 
 
-bash train_sse_mcmc.sh CIFAR10 WideResNet28x10 1 ../models ../data cSGLD
+#bash train_sse_mcmc.sh CIFAR10 WideResNet28x10 1 ../models ../data cSGLD
