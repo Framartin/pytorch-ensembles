d4272f5e6064f8f6a9ca9054f11a95cad8519665
Martin G <1850174+Framartin@users.noreply.github.com>
2021-02-11 23:16:57 +0100

    move old scripts, rename final ones

--- train_sse_mcmc_rq_nb_samples.sh
+++ train_sse_mcmc_rq_nb_samples.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -l
 
+echo "*** Train 1 cSGLD for sample / cycle ***"
+
 # launch with:
-# bash train_sse_mcmc_rq_nb_samples.sh >>log/cifar10/train_sse_mcmc_rq_nb_samples.log 2>&1
+# bash train_sse_mcmc_rq_nb_samples2.sh >>log/cifar10/train_sse_mcmc_rq_nb_samples2.log 2>&1
 
 source /opt/miniconda/bin/activate
 conda activate pytorch
@@ -9,7 +11,7 @@ conda activate pytorch
 set -x
 
 #specify GPU
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=2
 
 DATASET="CIFAR10"
 ARCH="PreResNet110"
@@ -24,18 +26,24 @@ DATA_PATH="../data"
 #  CYCLE_SAVES=3
 #  CYCLES=34
 #  NOISE_EPOCHS=3
-INJECT_NOISE_OR_NOT="--inject_noise"
-MAX_LR=0.5
-CYCLE_EPOCHS=57
-CYCLE_SAVES=10
-CYCLES=5
-NOISE_EPOCHS=10
 
-WD=3e-4
+for NB_SAMPLES in {1..9} ; do
+  # skip training with 3 samples / cycles (already done)
+  if [ $NB_SAMPLES -eq 3 ]; then
+      continue
+  fi
 
-python sse_mcmc_train.py $INJECT_NOISE_OR_NOT \
-    --dir="${BASE_DIR}/${DATASET}/${ARCH}/cSGLD_cycles${CYCLES}_savespercycle${CYCLE_SAVES}_it${ITER}" \
-    --model="$ARCH" --dataset="$DATASET" --noise_epochs=$NOISE_EPOCHS --data_path="$DATA_PATH" \
-    --alpha=1 --cycles=$CYCLES --iter="$ITER" \
-    --cycle_epochs=$CYCLE_EPOCHS --cycle_saves=$CYCLE_SAVES --max_lr=$MAX_LR --wd=$WD \
-    --device_id 0 --transform="NoNormalization"
+  INJECT_NOISE_OR_NOT="--inject_noise"
+  MAX_LR=0.5
+  CYCLE_EPOCHS=$(( 47 + $NB_SAMPLES ))
+  CYCLES=5
+  WD=3e-4
+
+  python sse_mcmc_train.py $INJECT_NOISE_OR_NOT \
+      --dir="${BASE_DIR}/${DATASET}/${ARCH}/RQ/cSGLD_cycles${CYCLES}_savespercycle${NB_SAMPLES}" \
+      --model="$ARCH" --dataset="$DATASET" --noise_epochs="$NB_SAMPLES" --data_path="$DATA_PATH" \
+      --alpha=1 --cycles=$CYCLES --iter="$ITER" \
+      --cycle_epochs=$CYCLE_EPOCHS --cycle_saves="$NB_SAMPLES" --max_lr=$MAX_LR --wd=$WD \
+      --device_id 0 --transform="NoNormalization"
+
+done
